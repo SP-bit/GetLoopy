5f4e16891f1c49a116c8149cc4ffb2e1780e9e19
Sathwik <ps.bits5@gmail.com>
2024-10-13 17:53:38 -0400

    Part A Task 3 completed

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main
 {
     public static void main(String[] args)
     {
-        for(int count=30; count>=0; count-- )
+        for(int count=0; count<=18; count += 3 )
         {
             System.out.print(count + " ");
         }
